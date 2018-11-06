ec904d91aa0fabeef7d617332c8c6d81450251e3
Precious Humphrey <phumphrey23@gmail.com>
2018-11-06 17:51:47 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+
+  def call(env)
+    resp.write time.now
+
+    if time.now.hour < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+
+    resp.finish
+  end
+
+end
